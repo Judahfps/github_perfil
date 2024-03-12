3316cf54704ca2aa2dd89d466253a8abc86c12ef
Guilherme Silva <sebastian_49@hotmail.com>
2024-03-12 06:51:35 -0400

    ajuste da do alt do avatar

--- src/compenets/Perfil/index.jsx
+++ src/compenets/Perfil/index.jsx
@@ -7,7 +7,7 @@ const Perfil = ({nomeUsuario}) => {
     }
     return(
         <header className={styles.header}>
-            <img className={styles.avatar} src= {`./src/compenets/Perfil/imagens/gnome.jpeg`} alt="Jupter"/>
+            <img className={styles.avatar} src= {`./src/compenets/Perfil/imagens/gnome.jpeg`}/>
             <h1 className={styles.name}>
                 {nomeUsuario}
                 </h1>
